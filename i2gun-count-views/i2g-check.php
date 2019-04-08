5cc73ecc02413d76d3eb0fbdeb6da53ac10ac988
ermeck <ermeck@bk.ru>
2019-04-08 21:50:20 +0400

    testing commit

---
+++ i2gun-count-views/i2g-check.php
@@ -0,0 +1,12 @@
+<?php
+
+function i2g_check_field($column) {
+    global $wpdb;
+    $fields = $wpdb->get_results("SHOW fields FROM $wpdb->posts", ARRAY_A);
+    foreach ($fields as $field) {
+        if ( $field['Field'] == $column ) {
+            return true;
+        }
+    }
+    return false;
+}
